41f383fa31e34b7e57b6cba6688fd7c0cae4bd55
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-03-16 15:01:22 +0100

    treebox no longer works on linux

--- content/bulkimport.ts
+++ content/bulkimport.ts
@@ -1,9 +1,12 @@
-declare var Zotero: any // eslint-disable-line no-var
+// declare var Zotero: any // eslint-disable-line no-var
 
 type XULElement = HTMLElement & {
   selectedItem?: HTMLElement
   columns?: any
   view?: any
+  options?: any
+  selected?: string
+  value?: string
 }
 let ui: Record<string, XULElement> = {}
 let extensions: string[]
@@ -15,7 +18,7 @@ let params: {
   progress: any
 }
 
-const xul = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul'
+const html = 'http://www.w3.org/1999/xhtml'
 
 function onLoad() { // eslint-disable-line @typescript-eslint/no-unused-vars
   params = (window as any).arguments[0]
@@ -25,17 +28,16 @@ function onLoad() { // eslint-disable-line @typescript-eslint/no-unused-vars
     link: document.getElementById('folder-import-link'),
   }
 
-  const rows = document.getElementById('folder-import-extensions-rows')
-
   extensions = Array.from(params.extensions).sort().filter((ext: string) => ext !== 'lnk')
   const defaults = [ 'pdf', 'docx', 'html', 'odt' ].filter(ext => extensions.includes(ext))
   extensions = defaults.concat(extensions.filter(ext => !defaults.includes(ext)))
 
   for (const ext of extensions) {
-    const ti = rows.appendChild(document.createElementNS(xul, 'treeitem'))
-    const tr = ti.appendChild(document.createElementNS(xul, 'treerow'))
-    tr.appendChild(document.createElementNS(xul, 'treecell')).setAttribute('value', defaults.includes(ext) ? 'true' : 'false')
-    tr.appendChild(document.createElementNS(xul, 'treecell')).setAttribute('label', ext)
+    const option = ui.extensions.appendChild(document.createElementNS(html, 'option'))
+    option.textContent = ext
+    option.setAttribute('value', ext)
+    option.setAttribute('class', 'checkbox-multiselect')
+    if (defaults.includes(ext)) option.setAttribute('selected', '')
   }
 
   ui.mode.selectedItem = document.getElementById('folder-import-import')
@@ -49,12 +51,7 @@ function onCancel() { // eslint-disable-line @typescript-eslint/no-unused-vars
 
 function onAccept() { // eslint-disable-line @typescript-eslint/no-unused-vars
   params.link = ui.mode && ui.mode.selectedItem === ui.link
-  params.extensions = new Set
-  for (let i = 0; i < ui.extensions.view.rowCount; i++) {
-    const checked = ui.extensions.view.getCellValue(i, ui.extensions.columns.getColumnAt(0)) === 'true'
-    const ext = extensions[i]
-    Zotero.debug(`selected: ${JSON.stringify({ checked, ext })}`)
-    if (checked) params.extensions.add(ext)
-  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-return
+  params.extensions = new Set(Array.from(ui.extensions.options).filter((opt: XULElement) => opt.selected).map((opt: XULElement) => opt.value))
   return true
 }
